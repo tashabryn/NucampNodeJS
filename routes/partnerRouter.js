8790d5dc93db2079307adb1872c72924a40acf3a
Tasha McAllister <tashabmcallister@gmail.com>
2021-07-26 15:05:38 -0500

    Updated Week 1 Assignment

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -34,22 +34,22 @@ partnerRouter.route('/:partnerId')
 })
 
 .get((req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+    res.end(`Will send details of the partner: ${req.params.partnerId} to you`);
 }) 
 
 .post((req, res) => {
     res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+    res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
 })
 
 .put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} 
+    res.write(`Updating the partner: ${req.params.partnerId}\n`);
+    res.end(`Will update the partner: ${req.body.name} 
         with description: ${req.body.description}`);
 })
 
 .delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+    res.end(`Deleting partner: ${req.params.partnerId}`);
 });
 
 module.exports = partnerRouter;
