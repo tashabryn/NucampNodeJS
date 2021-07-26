4b239488ac84e797e8352f83883c65cdd5842522
Tasha McAllister <tashabmcallister@gmail.com>
2021-07-26 14:55:46 -0500

    Week 1 Assignment Update

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -9,20 +9,20 @@ partnerRouter.route('/')
 })
 
 .get((req, res) => {
-    res.end('Will send all the campsites to you');
+    res.end('Will send all the partners to you');
 })
 
 .post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
 })
 
 .put((req, res) => {
    res.statusCode = 403;
-   res.end('PUT operation not supported on /campsites'); 
+   res.end('PUT operation not supported on /partners'); 
 })
 
 .delete((req, res) => {
-    res.end('Deleting all campsites'); 
+    res.end('Deleting all partners'); 
 });
 
 
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -9,20 +9,20 @@ promotionRouter.route('/')
 })
 
 .get((req, res) => {
-    res.end('Will send all the campsites to you');
+    res.end('Will send all the promotions to you');
 })
 
 .post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+    res.end(`Will add the promotion: ${req.body.name} with description: ${req.body.description}`);
 })
 
 .put((req, res) => {
    res.statusCode = 403;
-   res.end('PUT operation not supported on /campsites'); 
+   res.end('PUT operation not supported on /promotions'); 
 })
 
 .delete((req, res) => {
-    res.end('Deleting all campsites'); 
+    res.end('Deleting all promotions'); 
 });
 
 
